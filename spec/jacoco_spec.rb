70be2aa848466997cbebf1cf5fb69e3c35aeaf0b
Vitalii Nikonorov <nik.vitaly@mail.ru>
2019-11-15 12:09:50 +0700

    Replaced quote by single-quote

--- spec/jacoco_spec.rb
+++ spec/jacoco_spec.rb
@@ -1,16 +1,16 @@
-require File.expand_path("../spec_helper", __FILE__)
+require File.expand_path('../spec_helper', __FILE__)
 
 module Danger
 
   describe Danger::DangerJacoco do
-    it "should be a plugin" do
+    it 'should be a plugin' do
       expect(Danger::DangerJacoco.new(nil)).to be_a Danger::Plugin
     end
 
     #
     # You should test your custom attributes and methods here
     #
-    describe "with Dangerfile" do
+    describe 'with Dangerfile' do
       before do
         @dangerfile = testing_dangerfile
         @my_plugin = @dangerfile.jacoco
@@ -27,24 +27,23 @@ module Danger
         path_a = "#{File.dirname(__FILE__)}/fixtures/output_a.xml"
 
         @my_plugin.minimum_project_coverage_percentage = 50
-        @my_plugin.minimum_class_coverage_map = { "com/example/CachedRepository" => 100 }
+        @my_plugin.minimum_class_coverage_map = { 'com/example/CachedRepository' => 100 }
 
         @my_plugin.report path_a
 
-        expect(@dangerfile.status_report[:errors]).to eq(["Total coverage of 32.9%. Improve this to at least 50%",
-                                                          "Class coverage is below minimum. Improve to at least 0%"])
-        expect(@dangerfile.status_report[:markdowns][0].message).to include("### JaCoCO Code Coverage 32.9% :warning:")
-        expect(@dangerfile.status_report[:markdowns][0].message).to include("| Class | Covered | Meta | Status |")
-        expect(@dangerfile.status_report[:markdowns][0].message).to include("|:---|:---:|:---:|:---:|")
-        expect(@dangerfile.status_report[:markdowns][0].message).to include("| `com/example/CachedRepository` | 50% | 100% | :warning: |")
-
+        expect(@dangerfile.status_report[:errors]).to eq(['Total coverage of 32.9%. Improve this to at least 50%',
+                                                          'Class coverage is below minimum. Improve to at least 0%'])
+        expect(@dangerfile.status_report[:markdowns][0].message).to include('### JaCoCO Code Coverage 32.9% :warning:')
+        expect(@dangerfile.status_report[:markdowns][0].message).to include('| Class | Covered | Meta | Status |')
+        expect(@dangerfile.status_report[:markdowns][0].message).to include('|:---|:---:|:---:|:---:|')
+        expect(@dangerfile.status_report[:markdowns][0].message).to include('| `com/example/CachedRepository` | 50% | 100% | :warning: |')
       end
 
       it 'test with package coverage' do
         path_a = "#{File.dirname(__FILE__)}/fixtures/output_a.xml"
 
         @my_plugin.minimum_project_coverage_percentage = 50
-        @my_plugin.minimum_package_coverage_map = { "com/example/" => 70 }
+        @my_plugin.minimum_package_coverage_map = { 'com/example/' => 70 }
 
         @my_plugin.report path_a
 
@@ -57,8 +56,8 @@ module Danger
 
         @my_plugin.minimum_project_coverage_percentage = 50
         @my_plugin.minimum_package_coverage_map = {
-          "com/example/" => 70,
-          "com/" => 90
+          'com/example/' => 70,
+          'com/' => 90
         }
 
         @my_plugin.report path_a
@@ -72,8 +71,8 @@ module Danger
 
         @my_plugin.minimum_project_coverage_percentage = 50
         @my_plugin.minimum_package_coverage_map = {
-          "com/example/" => 77,
-          "com/" => 30
+          'com/example/' => 77,
+          'com/' => 30
         }
 
         @my_plugin.report path_a
@@ -87,10 +86,10 @@ module Danger
 
         @my_plugin.minimum_project_coverage_percentage = 50
         @my_plugin.minimum_package_coverage_map = {
-          "com/example/" => 77,
-          "com/" => 30
+          'com/example/' => 77,
+          'com/' => 30
         }
-        @my_plugin.minimum_class_coverage_map = { "com/example/CachedRepository" => 100 }
+        @my_plugin.minimum_class_coverage_map = { 'com/example/CachedRepository' => 100 }
 
         @my_plugin.report path_a
 
@@ -103,10 +102,10 @@ module Danger
 
         @my_plugin.minimum_project_coverage_percentage = 50
         @my_plugin.minimum_package_coverage_map = {
-          "com/example/" => 90,
-          "com/" => 85
+          'com/example/' => 90,
+          'com/' => 85
         }
-        @my_plugin.minimum_class_coverage_map = { "com/example/CachedRepository" => 80 }
+        @my_plugin.minimum_class_coverage_map = { 'com/example/CachedRepository' => 80 }
 
         @my_plugin.report path_a
 
